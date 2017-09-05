880d32e63a20d55bbbe94898218a57fdfe7af537
baozuzu <984030850@qq.com>
2017-09-05 09:25:16 +0000

    Finish layout and routes

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,9 +1,17 @@
 <!DOCTYPE html>
 <html>
-<head>
-  <title>@yield('title','sample')-我的第一个Laravel</title>
-</head>
-<body>
-  @yield('content')
-</body>
+  <head>
+    <title>@yield('title', 'Sample App') - 我的第一个Laravel</title>
+    <link rel="stylesheet" href="/css/app.css">
+  </head>
+  <body>
+    @include('layouts._header')
+
+    <div class="container">
+      <div class="col-md-offset-1 col-md-10">
+        @yield('content')
+        @include('layouts._footer')
+      </div>
+    </div>
+  </body>
 </html>
